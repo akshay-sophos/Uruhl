c51bda4e8a45454f73de09a5ef86a6940cb36b8c
Akshay Sreekumar <akshaysreekumar1997@gmail.com>
2018-06-22 23:40:42 +0530

    Working
    Duty cycle seems to have no effect.
    How do I control action value now?

--- motor.py
+++ motor.py
@@ -16,10 +16,12 @@ APHASE = machine.Pin(25,machine.Pin.OUT)
 BPHASE = machine.Pin(17,machine.Pin.OUT)
 CONFIG = machine.Pin(5,machine.Pin.OUT)
 M1 = machine.Pin(27,machine.Pin.OUT)
-nSLEEP = machine.Pin(2,machine.Pin.IN)
+nSLEEP = machine.Pin(2,machine.Pin.OUT)
 nFAULT = machine.Pin(34,machine.Pin.IN)
 LED = machine.Pin(12,machine.Pin.OUT)
 CONFIG.value(0)
+nSLEEP.value(1)
+
 # In phase/enable mode, the xPHASE input pins control the direction of current flow through each H-bridge.
 #This sets the direction of rotation of a DC motor
 # Driving the xENBL input pins active high enables the H-bridge outputs. This can be used as PWM speed control of a DC motor
@@ -30,20 +32,30 @@ CONFIG.value(0)
 
 #When xENBL is 0, then M1 = 0 makes OUT = 0. Else OUT = z (slow decay or brake mode)
 
+pwmAENBL = machine.PWM(AENBL)
+pwmBENBL = machine.PWM(BENBL)
+#pwmLED = machine.PWM(LED)
+
 def blink(x):
     LED.value(1)
     utime.sleep_ms(x)
     LED.value(0)
     utime.sleep_ms(x)
+    # pwmLED.freq(1000-x)
+    # pwmLED.duty(40)
 
 def movA(dir):
     APHASE.value(dir)
-    AENBL.value(1)
+    # AENBL.value(1)
+    pwmAENBL.freq(10)
+    pwmAENBL.duty(100)
+    # print("Moving")
 
 def movB(dir):
     BPHASE.value(dir)
-    BENBL.value(1)
-
+    # BENBL.value(1)
+    pwmBENBL.freq(10)
+    pwmBENBL.duty(100)
 def brakeA():
     AENBL.value(0)
     M1.value(0)
@@ -61,38 +73,56 @@ def slowB():
     M1.value(1)
 
 def delay(x):
-    for i in range(x)
-
+    for i in range(x):
+        utime.sleep_ms(1)
 
 utime.sleep_ms(2000)
+
 while True:
-    if (nSLEEP == 0 or nFAULT == 0):
+    if (nFAULT == 0):
         print("Fault!!!")
         blink(100)
         break
-    blink(700)
-    print("Working")
-    #Run forward
+    #blink(700)
     movA(1)
-    movB(1)
-    delay(1000)
-    #Slow now
+    movB(0)
+    delay(50)
     slowA()
     slowB()
-    delay(1000)
-    #Run Backwards
+    delay(50)
     movA(0)
-    movB(0)
-    delay(1000)
-    #brake
-    brakeA()
-    brakeB()
-    delay(1000)
-    #MOve forward and slow wheel A
-    #Run forward
-    movA(1)
     movB(1)
-    delay(1000)
-    #Slow now
+    delay(50)
     slowA()
-    delay(1000)
+    slowB()
+    delay(50)
+    # print("Working")
+    # #Run forward
+    # movA(1)
+    # movB(0)
+    # delay(100)
+    # blink(700)
+    # #Slow now
+    # slowA()
+    # slowB()
+    # delay(500)
+    # #Run Backwards
+    # movA(0)
+    # movB(1)
+    # delay(100)
+    # blink(700)
+    # #brake
+    # brakeA()
+    # brakeB()
+    # delay(100)
+    # blink(700)
+    # #MOve forward and slow wheel A
+    # #Run forward
+    # movA(1)
+    # movB(0)
+    # delay(1000)
+    # blink(700)
+    # #Slow now
+    # slowA()
+    # delay(1000)
+    # blink(700)
